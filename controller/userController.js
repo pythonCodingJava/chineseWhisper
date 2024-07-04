5fc3dbf2b187c6620b0d8ba5db5f0fcfaeab43d4
Kartik <kartikgupta74665@gmail.com>
2024-07-04 14:00:25 +0530

    added vercel

--- controller/userController.js
+++ controller/userController.js
@@ -45,8 +45,7 @@ module.exports.login = async (req, res, next) => {
     res.cookie('uid',generateToken({Username:Username}), {
       httpOnly:true,
       secure:true,
-      sameSite:'none',
-      domain:process.env.ORIGIN
+      sameSite:'none'
     });
 
     return res.status(201).send(
